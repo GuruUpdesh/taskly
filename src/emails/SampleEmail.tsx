9314e5bd0e1baa6fcf36371033107e0bafda9297
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2023-12-03 16:40:54 -0800

    updated sample email

--- src/emails/SampleEmail.tsx
+++ src/emails/SampleEmail.tsx
@@ -3,11 +3,14 @@ import {
 	Button,
 	Container,
 	Head,
+	Heading,
 	Hr,
 	Html,
 	Img,
+	Link,
 	Preview,
 	Section,
+	Tailwind,
 	Text,
 } from "@react-email/components";
 import * as React from "react";
@@ -26,82 +29,57 @@ export const SampleEmail = ({ name = "User" }: SampleEmailProps) => (
 		<Preview>
 			Streamline your projects and collaborate better with Taskly.
 		</Preview>
-		<Body style={main}>
-			<Container style={container}>
-				<Img
-					src={`${baseUrl}/static/taskly-logo.png`}
-					width="170"
-					height="50"
-					alt="Taskly"
-					style={logo}
-				/>
-				<Text style={paragraph}>Hi {name},</Text>
-				<Text style={paragraph}>
-					Welcome to Taskly, the project management tool that empowers
-					you to streamline projects and collaborate effectively.
-				</Text>
-				<Section style={btnContainer}>
-					<Button style={button} href="https://tasklypm.com">
-						Discover Taskly
-					</Button>
-				</Section>
-				<Text style={paragraph}>
-					Cheers,
-					<br />
-					The Taskly Team
-				</Text>
-				<Hr style={hr} />
-				<Text style={footer}>
-					123 Project Ave - Project City, PC 12345
-				</Text>
-			</Container>
-		</Body>
+		<Tailwind>
+			<Body className="mx-auto my-auto bg-white font-sans">
+				<Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+					<Section className="mt-[32px]">
+						<Img
+							src={`${baseUrl}/static/taskly-dark.png`}
+							width="100"
+							height="38"
+							alt="Taskly"
+							className="mx-auto my-0"
+						/>
+					</Section>
+					<Text className="text-[14px] leading-[24px] text-black">
+						Hello {name},
+					</Text>
+					<Text className="text-[14px] leading-[24px] text-black">
+						Welcome to <strong>Taskly</strong>, the project
+						management tool that empowers you to streamline projects
+						and collaborate effectively.
+					</Text>
+					<Section className="mb-[32px] mt-[32px] text-center">
+						<Button
+							className="rounded bg-[#000000] px-5 py-3 text-center text-[12px] font-semibold text-white no-underline"
+							href={`${baseUrl}/signup`}
+						>
+							Get Started
+						</Button>
+					</Section>
+					<Text className="text-[14px] leading-[24px] text-black">
+						or copy and paste this URL into your browser:{" "}
+						<Link
+							href={`${baseUrl}/signup`}
+							className="text-blue-600 no-underline"
+						>
+							tasklypm.com/signup
+						</Link>
+					</Text>
+					<Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+					<Text className="text-[12px] leading-[24px] text-[#666666]">
+						<Link
+							href={"https://www.tasklypm.com/"}
+							className="text-blue-600 no-underline"
+						>
+							TasklyPM.com
+						</Link>
+						, simplified project management.
+					</Text>
+				</Container>
+			</Body>
+		</Tailwind>
 	</Html>
 );
 
 export default SampleEmail;
-
-const main = {
-	backgroundColor: "#ffffff",
-	fontFamily:
-		'-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
-};
-
-const container = {
-	margin: "0 auto",
-	padding: "20px 0 48px",
-};
-
-const logo = {
-	margin: "0 auto",
-};
-
-const paragraph = {
-	fontSize: "16px",
-	lineHeight: "26px",
-};
-
-const btnContainer = {
-	textAlign: "center" as const,
-};
-
-const button = {
-	backgroundColor: "#5F51E8",
-	borderRadius: "3px",
-	color: "#fff",
-	fontSize: "16px",
-	textDecoration: "none",
-	textAlign: "center" as const,
-	display: "block",
-	padding: "12px",
-};
-
-const hr = {
-	borderColor: "#cccccc",
-	margin: "20px 0",
-};
-
-const footer = {
-	color: "#8898aa",
-	fontSize: "12px",
-};
