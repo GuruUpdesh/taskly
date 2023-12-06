8d2610d2bed771ff34834250ee6958cd57b6f0a9
Cameron Hollis <cameronjhollis@gmail.com>
2023-12-06 00:04:49 -0400

    added projects list to navbar

---
+++ src/components/navbar/project-menu-item.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import type { Project } from "~/server/db/schema";
+import {
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuTrigger,
+} from "~/components/ui/navigation-menu";
+
+type ProjectMenuItemProps = {
+	projects: Project[];
+};
+
+const ProjectMenuItem = ({ projects }: ProjectMenuItemProps) => {
+	return (
+		<NavigationMenuItem>
+			<NavigationMenuTrigger>Projects</NavigationMenuTrigger>
+			<NavigationMenuContent className="absolute left-0 top-0 w-full">
+				<ul className="grid w-[200px] gap-3 p-4 md:grid-cols-1">
+					{projects.map((project) => (
+						<NavigationMenuLink key={project.id}>
+							{project.name}
+						</NavigationMenuLink>
+					))}
+				</ul>
+			</NavigationMenuContent>
+		</NavigationMenuItem>
+	);
+};
+
+export default ProjectMenuItem;
