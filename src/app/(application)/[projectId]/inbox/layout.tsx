e4e973918827b1b2bb9efe4c288314f235548ee2
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-01-14 11:11:30 -0800

    Refactor inbox layout and notification item components

--- src/app/(application)/[projectId]/inbox/layout.tsx
+++ src/app/(application)/[projectId]/inbox/layout.tsx
@@ -23,8 +23,8 @@ export default function InboxLayout({ children }: Params) {
 				maxSize={25}
 				defaultSize={15}
 			>
-				<div className="min-h-screen px-4">
-					<header className="flex items-center justify-between gap-2 border-b pb-4 pt-4">
+				<div className="min-h-screen">
+					<header className="flex items-center justify-between gap-2 border-b p-4">
 						<h3 className="scroll-m-20 text-2xl font-bold tracking-tight">
 							Inbox
 						</h3>
@@ -38,7 +38,9 @@ export default function InboxLayout({ children }: Params) {
 						</div>
 					</header>
 					<section className="flex flex-col gap-2 py-2">
-						<NotificationItem />
+						{Array.from({ length: 10 }).map((_, i) => (
+							<NotificationItem key={i} id={String(i)} />
+						))}
 					</section>
 				</div>
 			</ResizablePanel>
--- src/app/(application)/[projectId]/inbox/notification-item.tsx
+++ src/app/(application)/[projectId]/inbox/notification-item.tsx
@@ -2,13 +2,15 @@
 
 import React, { useMemo } from "react";
 import { usePathname, useRouter } from "next/navigation";
-
-import { Button } from "~/components/ui/button";
-import { Card } from "~/components/ui/card";
 import { cn } from "~/lib/utils";
 
-const NotificationItem = () => {
-	const id = "test";
+import { Separator } from "~/components/ui/separator";
+
+type Props = {
+	id: string;
+};
+
+const NotificationItem = ({id}: Props) => {
 
 	const router = useRouter();
 	const pathname = usePathname();
@@ -22,27 +24,32 @@ const NotificationItem = () => {
 	function handleClick() {
 		if (active) return;
 
-		router.push(`inbox/${id}`);
+		// get path before inbox
+		const path = pathname.split("inbox")[0];
+		router.push(`${path}/inbox/${id}`);
 	}
 
 	return (
-		<Card
-			onClick={handleClick}
-			className={cn("p-2 hover:bg-accent", active && "bg-accent")}
-		>
-			<div className="flex items-center justify-between">
-				<p className="font-semibold">Implement User Authentication</p>
-				<Button
-					className="rounded-full "
-					variant="outline"
-					size="iconSm"
-				></Button>
+		<>
+			<div
+				onClick={handleClick}
+				className={cn(
+					"rounded-none cursor-pointer p-2 hover:bg-accent",
+					active && "bg-accent",
+				)}
+			>
+				<div className="flex items-center justify-between">
+					<p className="font-semibold">
+						Implement User Authentication
+					</p>
+				</div>
+				<div className="flex justify-between">
+					<p className="text-muted-foreground">Asigned by Yash</p>
+					<p className="text-muted-foreground">1 day ago</p>
+				</div>
 			</div>
-			<div className="flex justify-between">
-				<p className="text-muted-foreground">Asigned by Yash</p>
-				<p className="text-muted-foreground">1 day ago</p>
-			</div>
-		</Card>
+			<Separator />
+		</>
 	);
 };
 
