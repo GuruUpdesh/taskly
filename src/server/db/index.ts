e56db065d74172ae980fb3cf8ba8d40fb09ea9b8
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-01-13 18:27:39 -0800

    followed guide on global var declaration and updated index.ts for db connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,24 +1,34 @@
 import { Client } from "@planetscale/database";
 import { drizzle as drizzlePS } from "drizzle-orm/planetscale-serverless";
-import { drizzle } from "drizzle-orm/mysql2";
+import { type MySql2Database, drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import { env } from "~/env.mjs";
 import * as schema from "./schema";
 
-async function createDbConnection() {
-	if (env.NODE_ENV === "development") {
+type devDbConnection = MySql2Database<Record<string, never>>
+
+declare global {
+	// eslint-disable-next-line no-var -- only var works here
+	var db: devDbConnection;
+}
+
+let db;
+
+if (env.NODE_ENV === "development") {
+	if (!global.db) {
 		const connection = await mysql.createConnection({
 			uri: env.DATABASE_URL,
 		});
-		return drizzle(connection);
-	} else {
-		const client = new Client({
-			url: env.DATABASE_URL,
-		});
-		return drizzlePS(client.connection(), { schema });
-	}
-}
 
-const db = await createDbConnection();
+		global.db = drizzle(connection);
+	}
+
+	db = global.db;
+} else {
+	const client = new Client({
+		url: env.DATABASE_URL,
+	});
+	db = drizzlePS(client.connection(), { schema });
+}
 
 export { db };
