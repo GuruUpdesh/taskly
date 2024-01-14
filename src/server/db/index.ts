fe283842b2a4fcce06c2abc5870c502e3e67d02f
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-01-13 18:37:50 -0800

    added logs

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -7,20 +7,26 @@ import * as schema from "./schema";
 
 type devDbConnection = MySql2Database<Record<string, never>>
 
+// Fix for to many client connections (dev only)
 declare global {
 	// eslint-disable-next-line no-var -- only var works here
-	var db: devDbConnection;
+	var db: devDbConnection | undefined;
 }
 
 let db;
 
 if (env.NODE_ENV === "development") {
+	console.log("Connecting to local database...");
 	if (!global.db) {
+		console.log("Creating new database connection");
 		const connection = await mysql.createConnection({
 			uri: env.DATABASE_URL,
 		});
+		console.log("Connected!");
 
 		global.db = drizzle(connection);
+	} else {
+		console.log("Using existing database connection");
 	}
 
 	db = global.db;
