0c3fd9222a40b4a4d60f9e06083db26a4c71bee3
Yash Sankanagouda <81050950+sankanay@users.noreply.github.com>
2024-01-25 17:27:58 -0800

    Refactor Invite Functionality (#62)
    * Redirect users to project page if they are part of it
    
    * Refactor Invite Functionality
    
    * Add Guru Updesh Singh as a contributor
    
    ---------
    
    Co-authored-by: Guru Updesh Singh <guruupdeshsingh@gmail.com>
    Co-authored-by: Guru Updesh Singh <62634868+GuruUpdesh@users.noreply.github.com>

--- src/app/(application)/settings/project/[projectId]/invite/page.tsx
+++ src/app/(application)/settings/project/[projectId]/invite/page.tsx
@@ -1,9 +1,4 @@
-import { auth } from "@clerk/nextjs";
-import dynamic from "next/dynamic";
-import { createInvite } from "~/actions/invite-actions";
-const InviteLink = dynamic(() => import("~/components/invite/invite-link"), {
-	ssr: false,
-});
+import InviteLinkWrapper from "~/components/invite/invite-link-wrapper";
 
 type Params = {
 	params: {
@@ -11,19 +6,8 @@ type Params = {
 	};
 };
 
-export default async function ProjectSettingsInvite({
+export default function ProjectSettingsInvite({
 	params: { projectId },
 }: Params) {
-	const { userId }: { userId: string | null } = auth();
-	if (!userId) return null;
-	const inviteLink = await createInvite(userId, projectId);
-	if (inviteLink === false) return null;
-
-	return (
-		<div className="container flex flex-col pt-4">
-			<h1>Invite with Link</h1>
-			<p>Invite a user using the link below!</p>
-			<InviteLink inviteLink={inviteLink} />
-		</div>
-	);
+	return <InviteLinkWrapper projectId={projectId} />;
 }
--- src/components/invite/invite-link.tsx
+++ src/components/invite/invite-link.tsx
@@ -2,16 +2,15 @@
 
 import React from "react";
 import { Button } from "../ui/button";
-import { Input } from "../ui/input";
 import { CopyIcon } from "lucide-react";
 import { throwClientError } from "~/utils/errors";
 
 type Props = {
-	inviteLink: string;
+	token: string;
 };
 
-function InviteLink({ inviteLink }: Props) {
-	const link = window.location.host + "/join/" + inviteLink;
+function InviteLink({ token }: Props) {
+	const link = window.location.host + "/join/" + token;
 	const handleCopyToClipboard = async () => {
 		try {
 			await navigator.clipboard.writeText(link);
@@ -21,8 +20,8 @@ function InviteLink({ inviteLink }: Props) {
 		}
 	};
 	return (
-		<div className="flex items-start">
-			<Input value={link} />
+		<div className="flex items-start gap-2">
+			<p className="flex-1 rounded-sm border p-2">{link}</p>
 			<Button onClick={handleCopyToClipboard} className="gap-1">
 				<CopyIcon className="h-4 w-4" />
 				Copy
