e987eed065aaf28725b657e7c3b467a9fcd39afd
Yash Sankanagouda <81050950+sankanay@users.noreply.github.com>
2024-01-28 16:29:14 -0800

    Update Account Creation Flow (#64)
    * Create page for new users to join existing project or create new project
    
    * Update Account Creation Flow
    
    * Add Guru Updesh Singh as a contributor
    
    ---------
    
    Co-authored-by: Guru Updesh Singh <guruupdeshsingh@gmail.com>

--- src/app/(application)/project/page.tsx
+++ src/app/(application)/project/page.tsx
@@ -1,12 +1,16 @@
 import ProjectCreateForm from "~/components/projects/create-form";
 import { initAction } from "~/actions/project-actions";
+import ProjectJoinForm from "~/components/projects/join-form";
 
 export default async function SettingsOverviewPage() {
 	await initAction();
 
 	return (
 		<div className="flex justify-center pt-24">
-			<ProjectCreateForm />
+			<div>
+				<ProjectCreateForm />
+				<ProjectJoinForm />
+			</div>
 		</div>
 	);
 }
