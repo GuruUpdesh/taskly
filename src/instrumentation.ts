68c914fcdbc3854e86e3ee0253d9a6c842f36a5c
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-02-24 15:53:18 -0800

    implemented auto sync of users on npm run dev

---
+++ src/instrumentation.ts
@@ -0,0 +1,43 @@
+import { env } from "~/env.mjs";
+import { db } from "~/server/db";
+import { users } from "~/server/db/schema";
+import { z } from "zod";
+
+const clerkUserSchema = z.object({
+	id: z.string(),
+	username: z.string().nullable(),
+	image_url: z.string(),
+});
+
+const dataSchema = z.array(clerkUserSchema);
+
+export async function register() {
+	if (env.NODE_ENV !== "development") {
+		return;
+	}
+	const result = await fetch("https://api.clerk.com/v1/users", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${env.CLERK_SECRET_KEY}`,
+		},
+	});
+
+	if (result.status !== 200) {
+		throw new Error("Failed to fetch users from Clerk");
+	}
+
+	const data = await result.json() as unknown;
+
+	const returnData = dataSchema.parse(data);
+
+	const usersData = returnData.map((user) => ({
+		userId: user.id,
+		username: user.username ?? "unknown",
+		profilePicture: user.image_url,
+	}));
+
+	await db.delete(users);
+
+	await db.insert(users).values(usersData);
+}
