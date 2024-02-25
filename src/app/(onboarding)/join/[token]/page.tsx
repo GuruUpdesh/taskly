54d551cee5083612aca53541d653d60566922bcf
Yash Sankanagouda <sankanay@oregonstate.edu>
2024-02-25 12:15:56 -0800

    debugging invite

--- src/app/(onboarding)/join/[token]/page.tsx
+++ src/app/(onboarding)/join/[token]/page.tsx
@@ -12,6 +12,7 @@ export default async function ProjectSettingsInvite({
 	params: { token },
 }: Params) {
 	const decodedToken = decodeURIComponent(token);
+	console.log(decodedToken, token);
 	const result = await joinProject(decodedToken);
 
 	if (result.success && result.projectId) {
