fc2696f184a8985aa8d3fd451b2c2ce8fa1a76c4
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-03-12 12:17:26 -0700

    removed some components and params from signup and signin

--- src/app/(onboarding)/(authentication)/sign-up/[[...signup]]/page.tsx
+++ src/app/(onboarding)/(authentication)/sign-up/[[...signup]]/page.tsx
@@ -1,14 +1,9 @@
-import { ClerkLoading, SignUp } from "@clerk/nextjs";
-
-import { Skeleton } from "~/components/ui/skeleton";
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
 	return (
 		<div>
-			<ClerkLoading>
-				<Skeleton className="h-[536px] w-[400px] rounded-sm bg-background" />
-			</ClerkLoading>
-			<SignUp path="/sign-up" routing="path" />
+			<SignUp />
 		</div>
 	);
 }
