93200f16e717a49bda7e1839731ed47b1d4f609d
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-05-06 10:41:23 -0700

    added www to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,17 +3,17 @@ import { type MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: `https://tasklypm.com/`,
+			url: `https://www.tasklypm.com/`,
 			lastModified: new Date(),
 			priority: 1,
 		},
 		{
-			url: `https://tasklypm.com/sign-in`,
+			url: `https://www.tasklypm.com/sign-in`,
 			lastModified: new Date(),
 			priority: 0.8,
 		},
 		{
-			url: `https://tasklypm.com/sign-up`,
+			url: `https://www.tasklypm.com/sign-up`,
 			lastModified: new Date(),
 			priority: 0.8,
 		},
