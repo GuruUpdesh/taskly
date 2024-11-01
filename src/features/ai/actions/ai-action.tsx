f19aa19d374e469600d6360c218b1dede64f52a8
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-10-31 17:23:29 -0700

    switched to structured format for ai task creation

--- src/features/ai/actions/ai-action.tsx
+++ src/features/ai/actions/ai-action.tsx
@@ -4,10 +4,10 @@ import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
-import { getAssigneesForProject } from "~/actions/project-actions";
-import { getSprintsForProject } from "~/actions/sprint-actions";
+import {
+	getCurrentSprintForProject,
+} from "~/actions/sprint-actions";
 import { env } from "~/env.mjs";
-import { getTaskAiSchema } from "~/features/ai/utils/ai-context";
 import { schemaValidators } from "~/features/tasks/config/taskConfigType";
 import { type User } from "~/server/db/schema";
 
@@ -77,67 +77,158 @@ const truncateDescription = (description: string, maxLength: number) => {
 	return description;
 };
 
-export async function aiGenerateTask(description: string, projectId: number) {
+export async function aiGenerateTask(
+	description: string,
+	projectId: number,
+	assignees: User[],
+) {
+	console.time("AI TASK CREATION");
 	if (await isAiLimitReached()) {
 		return;
 	}
 
-	const context = await getMostRecentTasks(projectId, 5);
-	const contextJSON = JSON.stringify(
-		context.map((task) => ({
-			...task,
-			description: truncateDescription(task.description, 100),
-		})),
+	// setup validator
+	const Tasks = z.array(
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			status: schemaValidators.status,
+			points: schemaValidators.points,
+			priority: schemaValidators.priority,
+			type: schemaValidators.type,
+			assignee: z.string(),
+			sprintId: z.string(),
+		}),
 	);
 
-	const assignees = await getAssigneesForProject(projectId);
-	if (assignees.error !== null) {
-		console.error(assignees.error);
-		return;
-	}
-	const sprints = await getSprintsForProject(projectId);
-
-	const taskSchema = getTaskAiSchema(assignees.data, sprints);
-
-	const prompt = `
-	RESPOND IN JSON FORMAT!
-	Create a new task for project management. Provide the following details:
-	Description of the task: ${description}
-	
-	If you feel this should be broken up into multiple tasks feel free to do so.
-	Please always return an array of tasks, even if it's just one task.
-
-	${taskSchema}
-
-	The most recent tasks are:
-	${contextJSON}
-
-	Note:
-		1. If the status is backlog, there cannot be a sprint.
-		2. If a sprint is selected, the status cannot be backlog.
-		3. The description can and should use basic markdown!
-			This includes: **bold**, #h1, ##h2, ###h3, *italic*, code (slanted quotes), <u>underlined</u>, [link](https://... "title), divider: ***
-	`;
-
-	console.log(prompt);
-
-	const gptResponse = await openai.chat.completions.create({
-		messages: [
-			{
-				role: "assistant",
-				content: prompt,
-			},
-		],
-		model: "gpt-4o",
+	const TaskReasoning = z.object({
+		tasks: Tasks,
 	});
 
-	if (!gptResponse.choices[0]?.message.content) {
-		return "";
+	// define the messages to be passed to open ai
+	const users = assignees.map((user) => user.username).join(", ");
+	const currentSprint = await getCurrentSprintForProject(projectId);
+	const systemMessage = `
+		You are an expert in creating tasks.
+		Depending on the complexity of the situation you can create multiple tasks.
+		Typically only do this if explicitly asked by the user.
+		Additional Rules:
+			* If the status is backlog, there cannot be a sprint.
+			* If a sprint is selected, the status cannot be backlog.
+			* The description can and should use Markdown to most effectively communicate information!
+		The potential values for assignee are: ${users}
+		If you assign a sprint it should be the current one: ${currentSprint?.id ?? "[error: there are no sprints]"}
+	`;
+
+	const context = await getMostRecentTasks(projectId, 5);
+	const formattedContext = context.map(
+		(task) =>
+			`${task.title}: ${task.status}, ${task.assignee}, ${task.sprintId}, ${task.points}. ${task.type} which is described as ${truncateDescription(task.description, 100)}`,
+	);
+	const contextMessage = `
+		Depending on the the users message you may want to reference the last 5 tasks created in this project.
+		${`[${formattedContext.join(",\n")}]`}
+	`;
+
+	// send all the data over with the response format
+	const completion = await openai.beta.chat.completions.parse({
+		model: "gpt-4o-2024-08-06",
+		messages: [
+			{
+				role: "system",
+				content: systemMessage,
+			},
+			{
+				role: "system",
+				content: contextMessage,
+			},
+			{
+				role: "user",
+				content: description,
+			},
+		],
+		response_format: zodResponseFormat(TaskReasoning, "TaskReasoning"),
+	});
+
+	try {
+		const taskReasoning = completion.choices[0]?.message.parsed;
+
+		if (!taskReasoning) {
+			console.error("tasks not returned from OpenAI");
+			return;
+		}
+
+		const tasks = taskReasoning.tasks;
+		console.timeEnd("AI TASK CREATION");
+		console.log(
+			`AI task creation. Prompt: ${description}:\n Result:`,
+			tasks,
+		);
+
+		return tasks;
+	} catch (e) {
+		console.error(e);
+		return;
 	}
+	// if (await isAiLimitReached()) {
+	// 	return;
+	// }
 
-	const jsonString = gptResponse.choices[0]?.message.content;
-	const jsonStart = jsonString.indexOf("[");
-	const jsonEnd = jsonString.lastIndexOf("]") + 1;
+	// const context = await getMostRecentTasks(projectId, 5);
+	// const contextJSON = JSON.stringify(
+	// 	context.map((task) => ({
+	// 		...task,
+	// 		description: truncateDescription(task.description, 100),
+	// 	})),
+	// );
 
-	return jsonString.substring(jsonStart, jsonEnd);
+	// const assignees = await getAssigneesForProject(projectId);
+	// if (assignees.error !== null) {
+	// 	console.error(assignees.error);
+	// 	return;
+	// }
+	// const sprints = await getSprintsForProject(projectId);
+
+	// const taskSchema = getTaskAiSchema(assignees.data, sprints);
+
+	// const prompt = `
+	// RESPOND IN JSON FORMAT!
+	// Create a new task for project management. Provide the following details:
+	// Description of the task: ${description}
+
+	// If you feel this should be broken up into multiple tasks feel free to do so.
+	// Please always return an array of tasks, even if it's just one task.
+
+	// ${taskSchema}
+
+	// The most recent tasks are:
+	// ${contextJSON}
+
+	// Additional Rules:
+	// 	* If the status is backlog, there cannot be a sprint.
+	// 	* If a sprint is selected, the status cannot be backlog.
+	// 	* The description can and should use Markdown to most effectively communicate information!
+	// `;
+
+	// console.log(prompt);
+
+	// const gptResponse = await openai.chat.completions.create({
+	// 	messages: [
+	// 		{
+	// 			role: "assistant",
+	// 			content: prompt,
+	// 		},
+	// 	],
+	// 	model: "gpt-4o",
+	// });
+
+	// if (!gptResponse.choices[0]?.message.content) {
+	// 	return "";
+	// }
+
+	// const jsonString = gptResponse.choices[0]?.message.content;
+	// const jsonStart = jsonString.indexOf("[");
+	// const jsonEnd = jsonString.lastIndexOf("]") + 1;
+
+	// return jsonString.substring(jsonStart, jsonEnd);
 }
--- src/features/ai/components/AiDialog.tsx
+++ src/features/ai/components/AiDialog.tsx
@@ -9,6 +9,7 @@ import { ChevronRight, Loader2, SparklesIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
+import { useShallow } from "zustand/react/shallow";
 
 import { createTask } from "~/actions/task-actions";
 import Message from "~/components/Message";
@@ -27,7 +28,6 @@ import { Textarea } from "~/components/ui/textarea";
 import { aiGenerateTask } from "~/features/ai/actions/ai-action";
 import { AIDAILYLIMIT, timeTillNextReset } from "~/features/ai/utils/aiLimit";
 import { useRegisterCommands } from "~/features/cmd-menu/registerCommands";
-import { schemaValidators } from "~/features/tasks/config/taskConfigType";
 import { taskNameToBranchName } from "~/features/tasks/utils/task-name-branch-converters";
 import { useRealtimeStore } from "~/store/realtime";
 import { useUserStore } from "~/store/user";
@@ -42,7 +42,9 @@ const formSchema = z.object({
 
 const AiDialog = ({ projectId }: Props) => {
 	const [open, setOpen] = useState(false);
-	const project = useRealtimeStore((state) => state.project);
+	const [project, assignees] = useRealtimeStore(
+		useShallow((state) => [state.project, state.assignees]),
+	);
 	const aiUsageCount = useUserStore((state) => state.aiUsageCount);
 
 	useRegisterCommands([
@@ -72,34 +74,18 @@ const AiDialog = ({ projectId }: Props) => {
 	});
 
 	async function onSubmit(values: z.infer<typeof formSchema>) {
-		const result = await aiGenerateTask(
+		const tasks = await aiGenerateTask(
 			values.description,
 			parseInt(projectId),
+			assignees,
 		);
 
-		if (!result) {
+		if (!tasks) {
 			toast.error(
 				`AI daily limit reached. Please try again in ${timeTillNextReset()} hours.`,
 			);
 			return;
 		}
-
-		const jsonResult = JSON.parse(result) as unknown;
-
-		const schema = z.array(
-			z.object({
-				title: schemaValidators.title,
-				description: schemaValidators.description,
-				status: schemaValidators.status,
-				points: schemaValidators.points,
-				priority: schemaValidators.priority,
-				type: schemaValidators.type,
-				assignee: schemaValidators.assignee,
-				sprintId: schemaValidators.sprintId,
-			}),
-		);
-
-		const tasks = schema.parse(jsonResult);
 
 		const createTasksPromises = tasks.map((task) =>
 			createTask({
