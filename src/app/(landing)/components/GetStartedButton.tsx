8f575b34b365863a0567bde9a884414fc0a19366
Guru Updesh Singh <guruupdeshsingh@gmail.com>
2024-11-07 17:39:23 -0800

    improved the structure of get started button

--- src/app/(landing)/components/GetStartedButton.tsx
+++ src/app/(landing)/components/GetStartedButton.tsx
@@ -1,32 +1,33 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 import { ArrowUpRightIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "~/components/ui/button";
 
 const GetStartedButton = () => {
 	return (
-		<Button
-			className="group relative z-10  rounded-full p-0 font-bold"
-			asChild
-		>
-			<Link href="/app">
-				<span className="z-10 flex h-full w-full items-center rounded-xl border bg-background-dialog px-2 text-foreground">
-					Get Started <ArrowUpRightIcon className="ml-4 h-5 w-5" />
-				</span>
-				<div className="absolute left-[-1px] top-[-1px] h-[calc(100%+2px)] w-[calc(100%+2px)] overflow-clip rounded-xl blur-md">
-					<div className="absolute left-0 top-0 h-[150%] w-full">
-						<img
-							className="absolute h-full w-full"
-							src="/static/auth-slow.gif"
-							alt="backdrop"
-						/>
-					</div>
+		<div className="relative">
+			<div className="absolute left-[-1px] top-[-1px] h-[calc(100%+2px)] w-[calc(100%+2px)] overflow-clip rounded-xl blur-md">
+				<div className="absolute left-0 top-0 h-[150%] w-full">
+					<Image
+						className="absolute h-full w-full"
+						src="/static/auth-slow.gif"
+						alt=""
+						fill
+					/>
 				</div>
-			</Link>
-		</Button>
+			</div>
+			<Button
+				className="group relative rounded-xl bg-background-dialog p-0 px-2 font-bold text-foreground hover:text-background-dialog border"
+				asChild
+			>
+				<Link href="/app">
+					Get Started <ArrowUpRightIcon className="ml-4 h-5 w-5" />
+				</Link>
+			</Button>
+		</div>
 	);
 };
 
